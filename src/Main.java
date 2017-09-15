16260eae6afab6e6448f4ad90a5a633892143369
Sundar Ram <sundarram7796@gmail.com>
2017-09-15 14:21:33 +0530

    Java files

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Student s[] = new Student();
+		StudentGroup sg[] = new StudentGroup();
+		s.Student(1001, " Alfred", "15/05/1997", 75);
+		s.Student(1002, "Dinesh", "15/05/1997", 80);
+		s.Student(1003, "Ragul", "30/06/1996", 65);
+		s.Student91004, "Ram", "07/07/1996", 85);
+		StudentGroup ss[] = new Student();
+		ss.getStudents();
+	}
+
+}
